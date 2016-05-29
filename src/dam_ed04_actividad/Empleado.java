ecd0db9c1ec6d6643487926afb2de9708ffb1f04
ajflorido <ajflorido@gmail.com>
2016-05-29 10:00:47 +0100

    Clase empleado en rama nueva

---
+++ src/dam_ed04_actividad/Empleado.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author User
+ */
+public class Empleado {
+    
+    private String DNI;
+    private String Nombre;
+    private String Apellidos;
+    
+    
+    
+    
+}
