8df4912a7286aa294d39186e6e37d1d971e660d7
ajflorido <ajflorido@gmail.com>
2016-05-29 10:03:11 +0100

    Agregados setters y getters

--- src/dam_ed04_actividad/Empleado.java
+++ src/dam_ed04_actividad/Empleado.java
@@ -14,8 +14,50 @@ public class Empleado {
     private String DNI;
     private String Nombre;
     private String Apellidos;
+
+    /**
+     * @return  DNI
+     */
+    public String getDNI() {
+        return DNI;
+    }
+
+    /**
+     * @param DNI
+     */
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    /**
+     * @return  Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre 
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return  Apellidos
+     */
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    /**
+     * @param Apellidos 
+     */
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
     
     
-    
+  
     
 }
