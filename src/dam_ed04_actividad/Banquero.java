ede25cf7d8609a0ddbd47dd3dbcc46b4ad72e310
max999acre <max999acre@gmail.com>
2016-05-29 10:05:15 +0100

    Fin de creacion de la clase banquero

--- src/dam_ed04_actividad/Banquero.java
+++ src/dam_ed04_actividad/Banquero.java
@@ -16,4 +16,68 @@ public class Banquero {
     private String dni;
     private boolean malaGente = true;
 
+    public Banquero(String nombre, String apellido, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the malaGente
+     */
+    public boolean isMalaGente() {
+        return malaGente;
+    }
+
+    /**
+     * @param malaGente the malaGente to set
+     */
+    public void setMalaGente(boolean malaGente) {
+        this.malaGente = malaGente;
+    }
+    
+    
+
 }
