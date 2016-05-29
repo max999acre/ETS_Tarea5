2d14f65971e7aed432391e16f59807d25923cb10
max999acre <max999acre@gmail.com>
2016-05-29 10:03:44 +0100

    Inicio de creacion de la clase banquero

---
+++ src/dam_ed04_actividad/Banquero.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author MXIII
+ */
+public class Banquero {
+    
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private boolean malaGente = true;
+
+}
