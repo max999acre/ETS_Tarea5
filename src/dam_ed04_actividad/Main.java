aedeaf9715e450005a43a99cdb98c8f72b5b9341
max999acre <max999acre@gmail.com>
2016-05-29 09:48:58 +0100

    Segundo commit de CarlosMR

--- src/dam_ed04_actividad/Main.java
+++ src/dam_ed04_actividad/Main.java
@@ -7,25 +7,41 @@ package dam_ed04_actividad;
 
 
 public class Main {
+    
+    private static final int RETIRAR = 0;
+    private static final int INGRESAR = 1;
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
 
         try {
-            miCuenta.retirar(2300);
+            operativa_cuenta(cuenta1, RETIRAR, 2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
+        
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            operativa_cuenta(cuenta1, INGRESAR, 695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
+        }
+        
+    }
+    
+    public static void operativa_cuenta(CCuenta cuenta, int operacion, float cantidad) throws Exception{
+        switch(operacion){
+            case RETIRAR:
+                cuenta.retirar(cantidad);
+                break;
+            case INGRESAR:
+                cuenta.ingresar(cantidad);
+                break;
         }
     }
 }
